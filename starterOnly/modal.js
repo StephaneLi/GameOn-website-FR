ed701c9b3ce1a08d7188a2b6f10dcacd408a3e68
Stéphane <stephane1920@gmail.com>
2021-11-22 18:02:28 +0100

    close #3 add inputs errors on submit modal form

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -7,17 +7,6 @@ function editNav() {
   }
 }
 
-function validRadioCity () {  
-  let validCity = false;
-  
-  cityCheckbox.forEach((elmnt) => {
-    // if 1 element is checked return true
-    if (elmnt.checked) {
-      validCity = true;
-    }
-  });
-  return validCity;
-}
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
@@ -25,9 +14,14 @@ const modalClose = document.querySelector('.close');
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
-// DOM Forms inputs
-const cityCheckbox = document.querySelectorAll('#select-city .checkbox-input');
-
+// DOM required Inputs
+const firstnameInput = document.querySelector('#first');
+const lastnameInput = document.querySelector('#last');
+const emailInput = document.querySelector('#email');
+const birthdateInput = document.querySelector('#birthdate');
+const quantityInput = document.querySelector('#quantity');
+const rules = document.querySelector('#checkbox1')
+const citiesCheckbox = document.querySelectorAll('#select-city .checkbox-input');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -38,19 +32,66 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+// close modal
 function closeModal() {
   modalbg.style.display = "none";
 }
 
 // validate modal form
 function validate() {
-  // All label are controlled by HTML5 validate rules
-  // Check if 1 radio is selected
-  const validRadio = validRadioCity();
+  let isValidForm = true
 
-  if(validRadio) {
-    return true;
-  } else {
-    return false;
-  }
+  // Init objects list with inputs elements and error messages 
+  const listInputsRequired = [
+    { node: firstnameInput, msg: "Veuillez entrer 2 caractères ou plus pour le champ du prénom.", valid: firstnameInput.validity.valid },
+    { node: lastnameInput, msg: "Veuillez entrer 2 caractères ou plus pour le champ du nom.", valid: lastnameInput.validity.valid },
+    { node: emailInput, msg: "Veuillez saisir une acresse mail valide.", valid: emailInput.validity.valid },
+    { node: birthdateInput, msg: "Vous devez entrer votre date de naissance.", valid: birthdateInput.validity.valid },
+    { node: quantityInput, msg: "Veuillez renseigner un chiffre entre 0 et 99.", valid: quantityInput.validity.valid },
+    { node: citiesCheckbox[0], msg: "Vous devez choisir une option.", valid: validRadioCity(citiesCheckbox) },
+    { node: rules, msg: "Vous devez vérifier que vous acceptez les termes et conditions.", valid: rules.validity.valid }
+  ];
+
+  // Add or Remove data-error attributes
+  listInputsRequired.forEach(function (elmnt) {
+    if( elmnt.valid === false ) {
+      addInputError(elmnt.node, elmnt.msg);
+    } else {
+      removeInputError(elmnt.node);
+    }
+  })
+
+  // find if add error in required inputs list 
+  listInputsRequired.find(function (elmnt) {
+    if(elmnt.valid === false) {
+      isValidForm = false;
+    }
+  })
+
+  return isValidForm;
+}
+
+// function add data attributes
+function addInputError (formData, msg) {
+  formData.parentNode.dataset.errorVisible = true;
+  formData.parentNode.dataset.error = msg;
+}
+
+// function remove data attributes
+function removeInputError (formData) {
+  delete formData.parentNode.dataset.errorVisible;
+  delete formData.parentNode.dataset.error;
+}
+
+// function check if 1 radio is selected
+function validRadioCity () {  
+  let validCity = false;
+  
+  citiesCheckbox.forEach((elmnt) => {
+    // if 1 element is checked return true
+    if (elmnt.checked) {
+      validCity = true;
+    }
+  });
+  return validCity;
 }
