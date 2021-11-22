6e0b45f78f594179a62b3f752043a48e0ba46533
Stéphane <stephane1920@gmail.com>
2021-11-22 21:10:08 +0100

    close #4 add message validation on submit, modify close classes to repeat action, add classes css alert-success

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -10,7 +10,10 @@ function editNav() {
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
-const modalClose = document.querySelector('.close');
+const modalClose = document.querySelectorAll('.close-event');
+const modalBody = document.querySelector(".modal-body");
+const modalForm = document.querySelector(".modal-body form");
+const modalAlert = document.querySelector(".modal-alert");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
 
@@ -20,12 +23,14 @@ const lastnameInput = document.querySelector('#last');
 const emailInput = document.querySelector('#email');
 const birthdateInput = document.querySelector('#birthdate');
 const quantityInput = document.querySelector('#quantity');
-const rules = document.querySelector('#checkbox1')
+const rules = document.querySelector('#checkbox1');
 const citiesCheckbox = document.querySelectorAll('#select-city .checkbox-input');
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
-modalClose.addEventListener("click", closeModal);
+modalClose.forEach(function (elmnt) {
+  elmnt.addEventListener("click", closeModal);
+})
 
 // launch modal form
 function launchModal() {
@@ -38,8 +43,9 @@ function closeModal() {
 }
 
 // validate modal form
-function validate() {
-  let isValidForm = true
+function validate(event) {
+  event.preventDefault();
+  let isValidForm = true;
 
   // Init objects list with inputs elements and error messages 
   const listInputsRequired = [
@@ -68,7 +74,19 @@ function validate() {
     }
   })
 
-  return isValidForm;
+  // hide modal form and replace by sucessfull message
+  // send formData with XMLHttpRequest
+  if(isValidForm) {
+    modalForm.classList.add("form-fadeout");
+    setTimeout(function() {
+      modalForm.classList.add("form-hidden");
+      modalForm.classList.remove("form-fadeout");
+      modalAlert.classList.add("alert-fadein");
+    }, 300)
+    sendData();
+  }
+  
+  return false;
 }
 
 // function add data attributes
@@ -93,5 +111,20 @@ function validRadioCity () {
       validCity = true;
     }
   });
+
   return validCity;
+}
+
+// function send data form 
+function sendData() {
+  const XHR = new XMLHttpRequest();
+
+  // Bind the FormData object and the form element
+  const formData = new FormData( modalForm );
+
+  // Set up our request
+  XHR.open( "GET", "./index.html" );
+
+  // The data sent is what the user provided in the form
+  XHR.send( formData );
 }
