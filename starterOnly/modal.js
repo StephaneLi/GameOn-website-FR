75b2f7ffa9f9ac88612e5d633ed156b706e9d270
Stéphane <stephane1920@gmail.com>
2021-11-23 09:09:52 +0100

    fix #2 create double verif JS HTML to prevent hacking

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -26,6 +26,9 @@ const quantityInput = document.querySelector('#quantity');
 const rules = document.querySelector('#checkbox1');
 const citiesCheckbox = document.querySelectorAll('#select-city .checkbox-input');
 
+// Form is send
+let formIsSend = false;
+
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
 modalClose.forEach(function (elmnt) {
@@ -40,53 +43,13 @@ function launchModal() {
 // close modal
 function closeModal() {
   modalbg.style.display = "none";
-}
 
-// validate modal form
-function validate(event) {
-  event.preventDefault();
-  let isValidForm = true;
-
-  // Init objects list with inputs elements and error messages 
-  const listInputsRequired = [
-    { node: firstnameInput, msg: "Veuillez entrer 2 caractères ou plus pour le champ du prénom.", valid: firstnameInput.validity.valid },
-    { node: lastnameInput, msg: "Veuillez entrer 2 caractères ou plus pour le champ du nom.", valid: lastnameInput.validity.valid },
-    { node: emailInput, msg: "Veuillez saisir une acresse mail valide.", valid: emailInput.validity.valid },
-    { node: birthdateInput, msg: "Vous devez entrer votre date de naissance.", valid: birthdateInput.validity.valid },
-    { node: quantityInput, msg: "Veuillez renseigner un chiffre entre 0 et 99.", valid: quantityInput.validity.valid },
-    { node: citiesCheckbox[0], msg: "Vous devez choisir une option.", valid: validRadioCity(citiesCheckbox) },
-    { node: rules, msg: "Vous devez vérifier que vous acceptez les termes et conditions.", valid: rules.validity.valid }
-  ];
-
-  // Add or Remove data-error attributes
-  listInputsRequired.forEach(function (elmnt) {
-    if( elmnt.valid === false ) {
-      addInputError(elmnt.node, elmnt.msg);
-    } else {
-      removeInputError(elmnt.node);
-    }
-  })
-
-  // find if add error in required inputs list 
-  listInputsRequired.find(function (elmnt) {
-    if(elmnt.valid === false) {
-      isValidForm = false;
-    }
-  })
-
-  // hide modal form and replace by sucessfull message
-  // send formData with XMLHttpRequest
-  if(isValidForm) {
-    modalForm.classList.add("form-fadeout");
-    setTimeout(function() {
-      modalForm.classList.add("form-hidden");
-      modalForm.classList.remove("form-fadeout");
-      modalAlert.classList.add("alert-fadein");
-    }, 300)
-    sendData();
+  if (formIsSend) {
+    modalForm.classList.remove("form-hidden");
+    modalForm.classList.remove("form-fadeout");
+    modalAlert.classList.remove("alert-fadein");
+    modalForm.reset();
   }
-  
-  return false;
 }
 
 // function add data attributes
@@ -115,6 +78,73 @@ function validRadioCity () {
   return validCity;
 }
 
+// function check characteres min 
+function checkInputChar (elmnt, minChar) {
+  let validJs = false;
+  // check HTML Valid
+  const validHtml = elmnt.validity.valid;
+  
+  // Check JS Valid
+  if( elmnt.value.length >= minChar) { 
+    validJs = true
+  }
+
+  if(validJs && validHtml) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// function check with regex constraint
+function checkInputRegex (elmnt, regex) {
+  let validJs = false;
+  // check HTML Valid  
+  const validHtml = elmnt.validity.valid;
+
+  // Check JS Valid Regex and not empty
+  if ( elmnt.value.length >= 0 && regex.test(elmnt.value)) { 
+    validJs = true
+  }
+
+  if (validJs && validHtml) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// function check input number 
+function checkInputNumber (elmnt) {
+  let validJs = false;
+  // check HTML Valid  
+  const validHtml = elmnt.validity.valid;
+
+  // Check JS Valid Regex and not empty
+  if (Number.isInteger(parseInt(elmnt.value))) { 
+    validJs = true
+  }
+
+  if (validJs && validHtml) {
+    return true
+  } else {
+    return false
+  }
+}
+
+// function check checkbox 
+function checkInputCheckbox (elmnt) {
+  let validJs = elmnt.checked;
+  // check HTML Valid  
+  const validHtml = elmnt.validity.valid;
+
+  if (validJs && validHtml) {
+    return true
+  } else {
+    return false
+  }
+}
+
 // function send data form 
 function sendData() {
   const XHR = new XMLHttpRequest();
@@ -127,4 +157,54 @@ function sendData() {
 
   // The data sent is what the user provided in the form
   XHR.send( formData );
+}
+
+// validate modal form
+function validate(event) {
+  event.preventDefault();
+  let isValidForm = true;
+
+  // Init objects list with inputs elements and error messages 
+  // Double verification : HTML and Javascript
+  const listInputsRequired = [
+    { node: firstnameInput, msg: "Veuillez entrer 2 caractères ou plus pour le champ du prénom.", valid: checkInputChar(firstnameInput, 2) },
+    { node: lastnameInput, msg: "Veuillez entrer 2 caractères ou plus pour le champ du nom.", valid: checkInputChar(lastnameInput, 2) },
+    { node: emailInput, msg: "Veuillez saisir une adresse mail valide.", valid: checkInputRegex(emailInput, /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/) },
+    { node: birthdateInput, msg: "Vous devez entrer votre date de naissance.", valid: checkInputRegex(birthdateInput, /^\d{4}[./-]\d{2}[./-]\d{2}$/) },
+    { node: quantityInput, msg: "Veuillez renseigner un chiffre entre 0 et 99.", valid: checkInputNumber(quantityInput) },
+    { node: citiesCheckbox[0], msg: "Vous devez choisir une option.", valid: validRadioCity(citiesCheckbox) },
+    { node: rules, msg: "Vous devez vérifier que vous acceptez les termes et conditions.", valid: checkInputCheckbox(rules) }
+  ];
+
+  // Add or Remove data-error attributes
+  listInputsRequired.forEach(function (elmnt) {
+    if( elmnt.valid === false ) {
+      addInputError(elmnt.node, elmnt.msg);
+    } else {
+      removeInputError(elmnt.node);
+    }
+  })
+
+  // find if add error in required inputs list 
+  listInputsRequired.find(function (elmnt) {
+    if(elmnt.valid === false) {
+      isValidForm = false;
+    }
+  })
+
+  // hide modal form and replace by sucessfull message
+  // send formData with XMLHttpRequest
+  if(isValidForm) {
+    modalForm.classList.add("form-fadeout");
+    const timer1 = setTimeout(function() {
+      modalForm.classList.add("form-hidden");
+      modalForm.classList.remove("form-fadeout");
+      modalAlert.classList.add("alert-fadein");
+      clearTimeout(timer1);
+    }, 300)
+    formIsSend = true;
+    sendData();
+  }
+  
+  return false;
 }
